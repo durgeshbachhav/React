a72c11d64bccf7d72be680d9d9dde8d652a750aa
durgeshbachhav <bachhavdurgesh123@gmail.com>
2023-09-24 19:06:46 +0530

    why useState hook?

--- 02_counter/src/App.jsx
+++ 02_counter/src/App.jsx
@@ -1,41 +1,42 @@
-import { useState } from 'react';
-import './App.css'
+import React from 'react'
+import { useState } from 'react'
 
-function App() {
-
+function App () {
   // propogate the changes in component
   // js se ui change nhi hota isliye state ka use hota hai jo ui mein changes karta hai
-  const [count, setCounter]=useState(0)
+  const [count,setCount] =useState(0)
+  // let count = 0
+const addValue=()=>{
+  if(count >= 0 && count < 20){
+    // setCount(count + 1)
+    // setCount(count + 1)
+    // setCount(count + 1)
+    // setCount(count + 1)
+
+    // setCount((prev)=> prev+1)
+    // setCount((prev)=> prev+1)
+    // setCount((prev)=> prev+1)
+    setCount((prev)=> prev+1)
+    // count+=1
+  }
+}
+const removeValue =()=>{
+  if(count >0 && count <= 20){
+    setCount(count - 1)
+    // count-=1
+  }
+}
   
-  const addValue =()=>{
-    if(count >= 0 && count <20){
-      // setCounter(count + 1)
-      // setCounter(count + 1)
-      // setCounter(count + 1)
-      // setCounter(count + 1)
-
-      // setCounter((prev)=>prev + 1)
-      // setCounter((prev)=>prev + 1)
-      setCounter((prev)=>prev + 1)
-    }
-    
-  }
-  const removeValue =()=>{
-    if(count > 0 && count <=20){
-      setCounter(count - 1)
-    }
-  }
-
   return (
-    <>
-      <h1>codehustle by durgesh</h1>
-      <h2>counter value : {count}</h2>
-      <button onClick={addValue}>add Value</button>
-      <br />
-      <button onClick={removeValue}>remove Value</button>
-
-    </>
+    <div>
+      <h1>codehustle by Durgesh</h1>
+      <h2>{count}</h2>
+      <button onClick={addValue}>addValue</button>
+      <button onClick={removeValue}>removeValue</button>
+    </div>
   )
 }
 
 export default App
+
+
