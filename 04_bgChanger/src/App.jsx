edf0c0ff11cd95fb722033fc6b03a4ed237d3dd5
durgeshbachhav <bachhavdurgesh123@gmail.com>
2023-09-26 21:22:57 +0530

    add title

--- 04_bgChanger/src/App.jsx
+++ 04_bgChanger/src/App.jsx
@@ -15,7 +15,8 @@ function App() {
       className="w-full h-screen duration-200"
       style={{ backgroundColor: color }}
     >
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <h2 className="font-bold text-4xl grid place-content-center py-10">click on btn and change backgroundColor</h2>
+      <div className="fixed flex flex-wrap  justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-4 py-2 rounded  cursor-pointer">
           <button
             className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
